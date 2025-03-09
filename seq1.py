012a0d340bce0ec0ee2d33ea282dd1651cb53c7a
Brian Bowman <brianbowman@mac.com>
2025-03-09 14:04:22 -0400

    add parms

--- seq1.py
+++ seq1.py
@@ -1,7 +1,8 @@
 import numpy as np
 def seq(l=0,h=1,c=10,m=10):
-    np.set_printoptions(threshold=999999)
+    # np.set_printoptions(threshold=999999)
     i = 1
+    print("Printing Sequence")
     while i < 10:
         seq = np.linspace(i, i+2, i*10)
         print("Sequence ", i, " :", seq, end="\r")
